960ecb93010108d0858cc73a13696b463224b0a7
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2023-12-20 13:55:32 +0100

    Remove redundant variables, add missing whitespace

--- src/maths.py
+++ src/maths.py
@@ -7,11 +7,9 @@ class Maths:
     @staticmethod
     def calculate_relative_distance(first : QPoint, second : QPoint) -> float:
         """Returns distance between two points"""
-        distance : float = sqrt((first[0] - second[0]) ** 2 + (first[1] - second[1]) ** 2)
-        return distance
+        return float(sqrt((first[0] - second[0]) ** 2 + (first[1] - second[1]) ** 2))
 
     @staticmethod
     def calculate_relative_vector(first : QPoint, second : QPoint) -> QVector2D:
         """Returns distance between two points"""
-        vector : QVector2D = (first[0] - second[0], first[1] - second[1])
-        return vector
+        return QVector2D((first[0] - second[0], first[1] - second[1]))
